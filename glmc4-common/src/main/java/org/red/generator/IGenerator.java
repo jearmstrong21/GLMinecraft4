9dd10fb048f7bee2c3ea6c58dd83aaf98a95e003
Unity123 <lucasgreer2@gmail.com>
2020-07-28 15:59:20 -0700

    Added some terrain gen, doesn't quite work yet

---
+++ glmc4-common/src/main/java/org/red/generator/IGenerator.java
@@ -0,0 +1,15 @@
+package org.red.generator;
+import java.lang.*;
+/**
+ * An interface for generic float generators.
+ */
+public interface IGenerator {
+
+    /**
+     * Generates a float value, designed for modular noise generators.
+     * @param coords Any number of coordinates, designed for noise-based generators.
+     * @return A float value, examples include the height of a block.
+     * @throws Exception May throw a generic exception.
+     */
+    public float generate(float... coords) throws Exception;
+}
