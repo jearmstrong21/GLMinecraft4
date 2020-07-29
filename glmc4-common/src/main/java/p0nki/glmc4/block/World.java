0f7b0a5a78bf1a583b90b460873406dcb1909cb7
jearmstrong21 <jearmstrong21@gmail.com>
2020-07-29 15:54:16 -0700

    Client world
    Signed-off-by: jearmstrong21 <jearmstrong21@gmail.com>

--- glmc4-common/src/main/java/p0nki/glmc4/block/World.java
+++ glmc4-common/src/main/java/p0nki/glmc4/block/World.java
@@ -3,6 +3,8 @@ package p0nki.glmc4.block;
 import org.joml.Vector2i;
 import org.joml.Vector3i;
 
+import java.util.List;
+
 public interface World {
 
     static Vector2i getCoordinateInChunk(Vector2i worldPos) {
@@ -29,5 +31,7 @@ public interface World {
     BlockState get(Vector3i blockPos);
 
     Chunk getChunk(Vector2i chunkCoordinate);
+
+    List<Vector2i> getLoadedChunks();
 
 }
