1c9cc2351d0d4cb3eff43ed8939b364fbbad508e
jearmstrong21 <jearmstrong21@gmail.com>
2020-07-29 17:45:28 -0700

    Accidentally push debug generation
    Signed-off-by: jearmstrong21 <jearmstrong21@gmail.com>

--- glmc4-common/src/main/java/p0nki/glmc4/block/Chunk.java
+++ glmc4-common/src/main/java/p0nki/glmc4/block/Chunk.java
@@ -1,10 +1,9 @@
 package p0nki.glmc4.block;
 
 import org.joml.Vector3i;
-import p0nki.glmc4.block.blocks.GrassBlock;
-import p0nki.glmc4.block.blocks.OakLogBlock;
 import p0nki.glmc4.network.PacketByteBuf;
-import p0nki.glmc4.utils.math.Axis;
+import p0nki.glmc4.wgen.Biome;
+import p0nki.glmc4.wgen.Biomes;
 import p0nki.glmc4.wgen.Generator;
 import p0nki.glmc4.wgen.SimplexNoiseGenerator;
 
@@ -23,28 +22,14 @@ public class Chunk implements PacketByteBuf.Equivalent {
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                BlockState b = Blocks.CACTUS.getDefaultState();
-                if (x == 0) b = Blocks.GRAVEL.getDefaultState();
-                else if (x == 1) b = Blocks.OAK_LEAVES.getDefaultState();
-                else if (x == 2) b = Blocks.OAK_LOG.getDefaultState().with(OakLogBlock.AXIS, Axis.X);
-                else if (x == 3) b = Blocks.OAK_LOG.getDefaultState().with(OakLogBlock.AXIS, Axis.Y);
-                else if (x == 4) b = Blocks.OAK_LOG.getDefaultState().with(OakLogBlock.AXIS, Axis.Z);
-                else if (x == 5) b = Blocks.OAK_PLANKS.getDefaultState();
-                else if (x == 6) b = Blocks.SAND.getDefaultState();
-                else if (x == 7) b = Blocks.GRASS.getDefaultState().with(GrassBlock.SNOWED, false);
-                else if (x == 8) b = Blocks.GRASS.getDefaultState().with(GrassBlock.SNOWED, true);
-                int y = Math.abs(z);
-                for (int i = 0; i <= y; i++) {
-                    c.set(x, i, z, b);
+                Biome b = Biomes.REGISTRY.get(biomes[x][z]).getValue();
+                int genX = x + (16 * cx);
+                int genZ = z + (16 * cz);
+                int y = (int) (8 + 4 * generator.generate(genX, genZ));
+                c.set(x, y, z, b.getTopBlockState());
+                for (int i = y - 1; i > 0; i--) {
+                    c.set(x, i, z, Blocks.STONE.getDefaultState());
                 }
-//                Biome b = Biomes.REGISTRY.get(biomes[x][z]).getValue();
-//                int genX = x + (16 * cx);
-//                int genZ = z + (16 * cz);
-//                int y = (int) (8 + 4 * generator.generate(genX, genZ));
-//                c.set(x, y, z, b.getTopBlockState());
-//                for (int i = y - 1; i > 0; i--) {
-//                    c.set(x, i, z, Blocks.STONE.getDefaultState());
-//                }
 
             }
         }
